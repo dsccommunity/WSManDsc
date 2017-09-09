15fe5794d376b344d314c78a0c32d5f7360a42b5
Daniel Scott-Raynsford <plagueho@gmail.com>
2017-09-09 18:28:26 +1200

    Improved RunAllTests.ps1

--- .vscode/RunAllTests.ps1
+++ .vscode/RunAllTests.ps1
@@ -1,10 +1,11 @@
-[System.String] $repoRoot = Split-Path -Path (Split-Path -Path $Script:MyInvocation.MyCommand.Path)
-if ( (-not (Test-Path -Path (Join-Path -Path $repoRoot -ChildPath 'DSCResource.Tests'))) -or `
-     (-not (Test-Path -Path (Join-Path -Path $repoRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
+[string] $repoRoot = Split-Path -Path (Split-Path -Path $Script:MyInvocation.MyCommand.Path)
+[string] $dscResourceTestsPath = Join-Path -Path $repoRoot -ChildPath 'Modules\WSManDsc\DSCResource.Tests'
+if ((-not (Test-Path -Path $dscResourceTestsPath)) -or `
+     (-not (Test-Path -Path (Join-Path -Path $dscResourceTestsPath -ChildPath 'TestHelper.psm1'))))
 {
     & git @('clone','https://github.com/PowerShell/DscResource.Tests.git',(Join-Path -Path $repoRoot -ChildPath 'Modules\WSManDsc\DSCResource.Tests\'))
 }
 
 Import-Module (Join-Path -Path $repoRoot -ChildPath 'Tests\TestHarness.psm1' -Resolve)
-$dscTestsPath = Join-Path -Path $repoRoot -ChildPath 'Modules\WSManDsc\DscResource.Tests\Meta.Tests.ps1'
+[string] $dscTestsPath = Join-Path -Path $dscResourceTestsPath -ChildPath 'Meta.Tests.ps1'
 Invoke-TestHarness -DscTestsPath $dscTestsPath
