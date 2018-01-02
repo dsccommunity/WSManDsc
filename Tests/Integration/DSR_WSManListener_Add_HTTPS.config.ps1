41778d7fc45531a85668edc9f7f9720c0b0e2b3a
Daniel Scott-Raynsford <plagueho@gmail.com>
2018-01-02 15:52:04 +1300

    Correct integration tests

--- Tests/Integration/DSR_WSManListener_Add_HTTPS.config.ps1
+++ Tests/Integration/DSR_WSManListener_Add_HTTPS.config.ps1
@@ -2,6 +2,7 @@ Configuration DSR_WSManListener_Config_Add_HTTPS {
     Import-DscResource -ModuleName WSManDsc
     node localhost {
         WSManListener Integration_Test {
+            Name                 = $Node.Name
             Transport      = $Node.Transport
             Ensure         = $Node.Ensure
             Port           = $Node.Port
--- Tests/Integration/DSR_WSManListener_Remove_HTTPS.config.ps1
+++ Tests/Integration/DSR_WSManListener_Remove_HTTPS.config.ps1
@@ -2,10 +2,11 @@ Configuration DSR_WSManListener_Config_Remove_HTTPS {
     Import-DscResource -ModuleName WSManDsc
     node localhost {
         WSManListener Integration_Test {
-            Transport      = $Node.Transport
-            Ensure         = $Node.Ensure
-            Port           = $Node.Port
-            Address        = $Node.Address
+            Name      = $Node.Name
+            Transport = $Node.Transport
+            Ensure    = $Node.Ensure
+            Port      = $Node.Port
+            Address   = $Node.Address
         }
     }
 }
