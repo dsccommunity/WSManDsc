388e631a36d49791163ae2c19a6110cb74d1eeed
Daniel Hughes <daniel.hughes@hetalogic.com>
2024-09-18 15:21:03 +0100

    `Get-InvalidOperationRecord` now provided by `DscResource.Test`

--- tests/TestHelpers/CommonTestHelper.psm1
+++ tests/TestHelpers/CommonTestHelper.psm1
@@ -43,44 +43,6 @@ function Get-InvalidArgumentRecord
     .PARAMETER ErrorRecord
         The error record containing the exception that is causing this terminating error
 #>
-function Get-InvalidOperationRecord
-{
-    [CmdletBinding()]
-    param
-    (
-        [ValidateNotNullOrEmpty()]
-        [System.String]
-        $Message,
-
-        [ValidateNotNull()]
-        [System.Management.Automation.ErrorRecord]
-        $ErrorRecord
-    )
-
-    if ($null -eq $Message)
-    {
-        $invalidOperationException = New-Object -TypeName 'InvalidOperationException'
-    }
-    elseif ($null -eq $ErrorRecord)
-    {
-        $invalidOperationException =
-            New-Object -TypeName 'InvalidOperationException' -ArgumentList @( $Message )
-    }
-    else
-    {
-        $invalidOperationException =
-            New-Object -TypeName 'InvalidOperationException' -ArgumentList @( $Message,
-                $ErrorRecord.Exception )
-    }
-
-    $newObjectParams = @{
-        TypeName = 'System.Management.Automation.ErrorRecord'
-        ArgumentList = @( $invalidOperationException.ToString(), 'MachineStateIncorrect',
-            'InvalidOperation', $null )
-    }
-    return New-Object @newObjectParams
-}
 
 Export-ModuleMember -Function `
-    Get-InvalidArgumentRecord, `
-    Get-InvalidOperationRecord
+    Get-InvalidArgumentRecord
