d54b23d561e7b4dc76d38e1ff6bdd1e1e7417d73
Dan Hughes <2237515+dan-hughes@users.noreply.github.com>
2025-06-24 12:13:57 +0100

    Add No port HTTP Listener test

---
+++ tests/Integration/DSC_WSManListener_Add_HTTP_NoPort.config.ps1
@@ -0,0 +1,10 @@
+Configuration DSC_WSManListener_Config_Add_HTTP {
+    Import-DscResource -ModuleName WSManDsc
+
+    node localhost {
+        WSManListener Integration_Test {
+            Transport = $Node.Transport
+            Ensure    = $Node.Ensure
+        }
+    }
+}
