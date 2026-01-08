3f7ffb1ff9906babd23ccd971d66ce32420128d2
Daniel Hughes <2237515+dan-hughes@users.noreply.github.com>
2026-01-08 16:57:42 +0000

    Fix test nesting

--- tests/Unit/Classes/WSManConfig.Tests.ps1
+++ tests/Unit/Classes/WSManConfig.Tests.ps1
@@ -315,15 +315,15 @@ Describe 'WSManConfig\Test()' -Tag 'Test' {
                         $script:getMethodCallCount += 1
                     }
             }
+        }
 
-            It 'Should return $true' {
-                InModuleScope -ScriptBlock {
-                    Set-StrictMode -Version 1.0
+        It 'Should return $true' {
+            InModuleScope -ScriptBlock {
+                Set-StrictMode -Version 1.0
 
-                    $script:mockInstance.Test() | Should -BeTrue
+                $script:mockInstance.Test() | Should -BeTrue
 
-                    $script:getMethodCallCount | Should -Be 1
-                }
+                $script:getMethodCallCount | Should -Be 1
             }
         }
     }
